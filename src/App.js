a71c9f5a6f08afacfac1dd9928b95e8b41f0eb6f
syedsameerpasha <sameerpashasyed3@gmail.com>
2023-08-15 11:08:40 +0530

    third  commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 
 import { useState } from 'react';
 import './App.css';
-//import About from './components/About';
+import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
-
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   const [modes,setModes]=useState("light");
@@ -19,16 +24,40 @@ function App() {
     }
   }
   return (
+    
     <>
+    <Router>
+    
+    
     <Navbar title='TextUtils' mode={modes} toggleMode={toggleMode}/>
     <div className='container my-3'>
-    <TextForm heading='Enter the text to analyze below'/>
-    {/*<About/>*/}
+    <Routes>
+          <Route  exact path="/about" element={<About/>}/>
+            
+          
+          
+          <Route exact path="/" element={<TextForm heading='Enter the text to analyze below'/>}/>
+          
+          
+        </Routes>
+      
+         
+   
+    
+    
+    
+    
+    
     </div>
+    </Router>
+    
       
       
     
     </>
+    
+
+
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,22 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = (props) => {
   return (
     <>
     <nav className={`navbar navbar-expand-lg navbar-$(props.mode) bg-${props.mode}`}>
   <div className="container-fluid ">
-    <a className="navbar-brand text-primary" href="/">{props.title}</a>
+    <Link className="navbar-brand text-primary" to="/">{props.title}</Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse " id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <a className="nav-link active text-primary" aria-current="page" href="/">Home</a>
+          <Link className="nav-link active text-primary" aria-current="page" to="/">Home</Link>
         </li>
         <li className="nav-item">
-          <a className="nav-link text-primary" href="/">About</a>
+          <Link className="nav-link text-primary" to="/about">About</Link>
         </li>
         </ul>
         
